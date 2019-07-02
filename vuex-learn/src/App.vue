2409df1afc17a3dc7d991d0a9b58b090ee27d49c
kondratskivd <kondratskivd@gmail.com>
2019-07-02 20:37:37 +0300

    add route in master #3

---
+++ vuex-learn/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="container container_users" >
+      <div class="btn-group mb-3" role="group" aria-label="Basic example">
+        <router-link class="btn btn-secondary" to="/">Home |</router-link>
+        <router-link class="btn btn-secondary" to="/test">| Test |</router-link>
+        <router-link class="btn btn-secondary" to="/learnvx">| Learn Vuex |</router-link>
+        <router-link class="btn btn-secondary" to="/car">| Car |</router-link>
+        <router-link class="btn btn-secondary" to="/ajax">| Ajax |</router-link>
+        <router-link class="btn btn-secondary" to="/top">| New Top </router-link>
+      </div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'app',
+  }
+</script>
+
+<style>
+
+  .container_users{
+    background: #ebebeb;
+    padding: 35px;
+  }
+</style>
